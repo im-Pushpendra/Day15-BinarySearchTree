660969e7aa3ffdc02da152ad474d08a5a703a5ad
im-Pushpendra <112095599+im-Pushpendra@users.noreply.github.com>
2023-01-16 20:12:31 +0530

    Binary Search Tree

---
+++ BinarySearchTree.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.day15.UC_Day15_BinarySearchTree;
+
+public class BinarySearchTree {
+
+	public static void main(String[] args) {
+		System.out.println("Binary Search\r\n"
+				+ "\r\n"
+				+ "Tree (BST)");
+
+	}
+
+}
